2af83560f9ec1ae18a2020a1462ea2114b89fcfa
leonfera <leoncioferreira47@gmail.com>
2026-01-10 13:19:40 -0300

    Correct success message and photo display logic
    Fix typo in success message and ensure proper display of photos.

--- Presente/presente.py
+++ Presente/presente.py
@@ -74,7 +74,8 @@ if st.button('Clique aqui para ver alguns momentos nossos') or st.session_state.
             col1.image(caminho_final, use_container_width=True)
         else:
             col2.image(caminho_final, use_container_width=True)
-    st.write("Te amarei até meu ultimo suspiro")
+    st.sucess("Te amarei até meu ultimo suspiro")
+
 
 
 
